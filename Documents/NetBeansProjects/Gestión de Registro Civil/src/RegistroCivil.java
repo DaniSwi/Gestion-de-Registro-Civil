9ddf1840d9bdeb0c2a96ca6849716ed0a6ba8078
Diego <Diego@LAPTOP-JCFI2K24>
2024-08-28 20:34:59 -0400

    Sout en main

--- Documents/NetBeansProjects/Gestión de Registro Civil/src/RegistroCivil.java
+++ Documents/NetBeansProjects/Gestión de Registro Civil/src/RegistroCivil.java
@@ -120,6 +120,8 @@ public class RegistroCivil {
     }
     
     public static void main(String args[]) throws IOException {
+        
+        System.out.println("Hola Chiquis!");
         HashMap mapaPersonasPorEdad = new HashMap();
         ArrayList listaPersonasMenoresEdad = new ArrayList();
         ArrayList listaPersonasMayoresEdad = new ArrayList();
